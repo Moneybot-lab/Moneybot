1a217fe790bdfc916e4c0926917740e40f1bd355
Moneybot-lab <troyreese1@msn.com>
2026-02-22 15:15:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
-All right I'm new at this so I can't find the tip line you want me to remove
+
 logging.basicConfig(level=logging.INFO)
 @app.route('/', methods=['GET'])
 def home():
